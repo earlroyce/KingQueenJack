ec3b9d5b349b2eb5ebda59efd5533024e04a3b26
earlroyce <44243688+earlroyce@users.noreply.github.com>
2018-11-16 14:11:08 -0600

    Initial Upload

---
+++ www/js/kingQueenJackBackup20181116.js
@@ -0,0 +1,190 @@
+/*
+ * Game Scripts by Earl Royce Hugo
+ * Shuffle Cards, Display, Remove
+ */
+
+var suitName = ["♠", "♣", "♥", "♦"],
+	cardFace = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+	deck = [],
+	deckNum = [],
+	shuffledDeck = [],
+	shuffledDeckNum = [],
+	graveyardDeck = [];
+	
+var matcher = -1;
+
+function arrange() {	
+	
+	deck = [];
+	deckNum = [];
+	graveyardDeck = [];
+	
+	// console.log("Arrange function called.");
+	// Build arranged deck with name
+	for (let suit of suitName) {
+		for (let face of cardFace) {
+			let card = face + " of " + suit;
+			deck.push(card);
+		}
+	}
+
+	// Build arranged deck with ordered pair
+	let x = 0, /* suit */
+		y = 0; /* face */
+		
+	for (x = 0; x < suitName.length; x++) {
+		for (y = 0; y < cardFace.length; y++) {
+			deckNum.push([x,y]);
+		}
+	}
+}
+
+arrange();
+
+var pile = deckNum.length,
+	grave = graveyardDeck.length;
+
+// console.log(deckNum);
+/*Shuffle Function
+ *Get Random Number between 0 and length
+ *Swap card in random number and position and card in last position
+ */
+ 
+//Shuffle Deck Labels
+function shuffle() {
+	arrange();
+	// console.log("Shuffle function started.")
+	
+	for (i = 0 ; i < 52; i++) {
+
+		let max = deck.length-1,
+			min = 0,
+			lastCard = deck[max];
+		
+		// var cardPos = Math.floor(Math.random() * (max - min + 1) ) + min;
+		var cardPos = randomize(min, max);
+		
+		deck[max] = deck[cardPos]
+		deck[cardPos] = lastCard;
+		
+		// console.log(deck.pop());
+		shuffledDeck.push(deck.pop());
+
+		if (deck.length == 0) {
+			// console.log(deck.pop());
+			// shuffledDeck.push(deck.pop());
+			break;
+		}
+	}
+}
+
+//Shuffle Deck Numbers
+function shuffleNum() {
+	// console.log("Shuffle function started.")
+	
+	arrange();
+	reset();
+	
+	for (i = 0 ; i < 52; i++) {
+
+		let max = deckNum.length-1,
+			min = 0,
+			lastCard = deckNum[max];
+		//	console.log (lastCard);
+		
+		// var cardPos = Math.floor(Math.random() * (max - min + 1) ) + min;
+		var cardPos = randomize(min, max);
+		
+		deckNum[max] = deckNum[cardPos]
+		deckNum[cardPos] = lastCard;
+		
+		// console.log(deck.pop());
+		shuffledDeckNum.push(deckNum.pop());
+
+		if (deck.length == 0) {
+			// console.log(deck.pop());
+			// shuffledDeck.push(deck.pop());
+			break;
+		}
+	}
+}
+
+// Draw Card
+function drawCard() {	
+	var drawnCard = shuffledDeck.pop();
+	graveyardDeck.push(drawnCard);
+	document.getElementById("display").innerHTML = drawnCard;
+	// console.log (graveyardDeck);
+}
+
+function drawCardNum() {
+	var drawnCard = shuffledDeckNum.pop(),
+		displayCard = [];
+	
+	graveyardDeck.push(drawnCard);
+	//displayCard = cardFace[drawncard[1]] + " of " + suitName[drawncard[0]];
+	
+	document.getElementById("display").innerHTML = drawnCard;
+	
+	pile -= 1;
+	grave += 1;
+			
+	document.getElementById("pilecount").innerHTML = pile;
+	document.getElementById("gravecount").innerHTML = grave;
+	
+	if (drawnCard[1] == matcher) {
+		document.getElementById("winner").style.display = "inline"
+		document.getElementById("winner").innerHTML = "You won!"
+		document.getElementById("drawer").disabled = true;
+		document.body.style.backgroundColor = "green";
+	}
+	
+	if (pile == 0) {
+		document.getElementById("drawer").disabled = true;
+	}
+}
+
+
+// Reset
+
+function reset () {
+
+	pile = deckNum.length;
+	grave = graveyardDeck.length;
+	
+	document.getElementById("pilecount").innerHTML = deckNum.length;
+	document.getElementById("gravecount").innerHTML = graveyardDeck.length;	
+
+	document.getElementById("winner").style.display = "none";
+	document.body.style.backgroundColor = "yellow";
+	
+}
+
+function clickKing(){
+	shuffleNum();
+	document.getElementById("display").innerHTML = "First to draw a King wins";
+	document.getElementById("drawer").disabled = false;
+	
+	return matcher = 12;
+}
+
+function clickQueen(){	
+	shuffleNum();
+	document.getElementById("display").innerHTML = "First to draw a Queen wins";
+	document.getElementById("drawer").disabled = false;
+	
+	return matcher = 11;
+}
+function clickJack(){
+	shuffleNum();
+	document.getElementById("display").innerHTML = "First to draw a Jack wins";
+	document.getElementById("drawer").disabled = false;
+	
+	return matcher = 10;
+}
+
+function randomize(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
